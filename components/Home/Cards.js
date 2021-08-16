5e1a473153f141cc1e8e34c215b0fdea57a38a37
Muhammad Ismail/IT/HO <ismail.muhammad@tcs.com.pk>
2021-08-16 05:19:36 -0700

    16-8-21

--- components/Home/Cards.js
+++ components/Home/Cards.js
@@ -1,35 +1,44 @@
 import Card from "./Card";
 import Link from "next/link";
+import Zoom from "react-reveal/Zoom";
 
 const Cards = () => {
   return (
     <div className="flex flex-col md:flex-row px-[2rem] justify-center items-center mt-[-3rem] z-[100]">
       <Link href="/tcs-kardo">
-        <a className="hover:z-50" data-aos="zoom-in" data-aos-delay="500">
-          <Card
-            title={"TCS Kardo"}
-            img={"/Home/tcsKardo.png"}
-            className="rounded-t-md md:rounded-none md:rounded-l-md"
-          />
+        <a className="hover:z-50">
+          <Zoom delay={500}>
+            <Card
+              title={"TCS Kardo"}
+              img={"/Home/tcsKardo.png"}
+              className="rounded-t-md md:rounded-none md:rounded-l-md"
+            />
+          </Zoom>
         </a>
       </Link>
       <Link href="/tcs-near-you">
-        <a className="hover:z-50" data-aos="zoom-in" data-aos-delay="600">
-          <Card title={"TCS Near You"} img={"/Home/tcsNearYou.png"} />
+        <a className="hover:z-50">
+          <Zoom delay={600}>
+            <Card title={"TCS Near You"} img={"/Home/tcsNearYou.png"} />
+          </Zoom>
         </a>
       </Link>
       <Link href="/rate-calculator">
-        <a className="hover:z-50" data-aos="zoom-in" data-aos-delay="700">
-          <Card title={"Rate Calculator"} img={"/Home/RateCalculator.png"} />
+        <a className="hover:z-50">
+          <Zoom delay={700}>
+            <Card title={"Rate Calculator"} img={"/Home/RateCalculator.png"} />
+          </Zoom>
         </a>
       </Link>
       <Link href="/">
-        <a className="hover:z-50" data-aos="zoom-in" data-aos-delay="800">
-          <Card
-            title={"TCS for Bussiness"}
-            img={"/Home/tcsForBussiness.png"}
-            className="rounded-b-md md:rounded-none md:rounded-r-md"
-          />
+        <a className="hover:z-50">
+          <Zoom delay={800}>
+            <Card
+              title={"TCS for Bussiness"}
+              img={"/Home/tcsForBussiness.png"}
+              className="rounded-b-md md:rounded-none md:rounded-r-md"
+            />
+          </Zoom>
         </a>
       </Link>
     </div>
